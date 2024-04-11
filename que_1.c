8a85f3849372615a1a6d6bbddc9ee93fa1d67304
kirtic0007 <157823349+kirtic0007@users.noreply.github.com>
2024-04-11 11:17:19 +0530

    Add files via upload

---
+++ que_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value of a=");   //print the output
+    scanf("%d",&a);     //for providing input from user
+    printf("Your entered value of a is: %d",a);
+
+}
